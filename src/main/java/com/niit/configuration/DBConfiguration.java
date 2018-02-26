51779c9a0012a3bbde290f4903c768a1d7a6d419
admin <admin@hp>
2018-02-27 00:05:43 +0530

    job completed

--- src/main/java/com/niit/configuration/DBConfiguration.java
+++ src/main/java/com/niit/configuration/DBConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.niit.model.User;
+import com.niit.model.Users;
 
 @Configuration
 @EnableTransactionManagement
@@ -30,7 +30,7 @@ public class DBConfiguration {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[]=new Class[]{User.class};//class objects of all entities
+		Class classes[]=new Class[]{Users.class};//class objects of all entities
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	@Bean
